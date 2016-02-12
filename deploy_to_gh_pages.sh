55da95d35960000cac45bc8c2bd6fb6b0996c985
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2016-02-12 14:55:03 +0200

    Initial support for Travis build

---
+++ deploy_to_gh_pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o pipefail
+(
+  set -e
+  set -x
+
+  mkdir deploy_gh_pages
+
+  git config user.name "Travis-CI"
+  git config user.email "travis@travis"
+
+  npm run swagger bundle --json -o deploy_gh_pages/swagger.json
+  npm run swagger bundle --yaml -o deploy_gh_pages/swagger.json
+
+  cd deploy_gh_pages
+  git add .
+  git commit -m "Deployed to Github Pages"
+
+  echo '=================================='
+  git config --get remote.origin.url
+  echo '=================================='
+  #git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages 2>&1
+) 2>&1 | sed "s/${GH_TOKEN}/xxPASSxx/"
